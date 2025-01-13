7edfa8286212dcd5514e9996a1e12bf82d3888e5
au683782 <84569264+odennis2@users.noreply.github.com>
2025-01-13 11:22:34 +0100

    firebase settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,22 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Roboto } from 'next/font/google'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcs_Xmk1lpdVH7FiPPrYHTWwhAIjKmJgY",
+  authDomain: "oscar-online-cv.firebaseapp.com",
+  projectId: "oscar-online-cv",
+  storageBucket: "oscar-online-cv.firebasestorage.app",
+  messagingSenderId: "119568961333",
+  appId: "1:119568961333:web:fd9e2a772bcf3f1cf9067c",
+  measurementId: "G-DWQ12JJZPY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const roboto = Roboto({
   subsets: ['latin'],
